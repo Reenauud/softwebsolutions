da1bf605995b0b859b869867dc1e78c9386cbe54
unknown <Korvo@DESKTOP-88FBGE6>
2025-03-06 14:55:46 +0100

    add style mobile

--- src/Views/Offres/SoftwareOffres/SoftwareOffres.jsx
+++ src/Views/Offres/SoftwareOffres/SoftwareOffres.jsx
@@ -13,30 +13,30 @@ import deploiement from "../../../../src/images/offreLogiciel/deploiement.jpg"
 function SoftwareOffres()
 {
 
-    return (  <div>
+    return (  <div className="ContainerSoftwareOffre">
         
 
         <h1>Création d'un Locigiel</h1>
 
-        <h6>- Réunion Initiale et Analyse des Besoins</h6>
+        <h3>Réunion Initiale et Analyse des Besoins</h3>
         <Cards
             img={reunion}
             alt={"ddd"}
-            txt={"Nous commençons par une réunion avec vous pour comprendre vos objectifs  et vos besoins spécifiques. Nous recueillons des informations sur votre  public cible et les fonctionnalités souhaitées pour votre site web."}
+            txt={"Nous commençons par une réunion avec vous pour comprendre vos objectifs  et vos besoins spécifiques. Nous recueillons des informations sur votre  public cible et les fonctionnalités souhaitées pour votre Logiciel."}
             height={82}
             width={"100%"}
             flex={"column"}
           ></Cards>
-           <h6>Conception de l’Architecture Logicielle</h6>
+           <h3>Conception de l’Architecture Logicielle</h3>
         <Cards
             img={conception}
             alt={"ddd"}
-            txt={"Nous créons un cahier des charges détaillé basé sur nos discussions  initiales. Nous proposons une structure de navigation et une  architecture technique adaptée à vos besoins."}
+            txt={"Nous définissons la structure globale du logiciel (modularité,  scalabilité). Nous choisissons les technologies et frameworks appropriés pour assurer une performance optimale."}
             height={82}
             width={"100%"}
             flex={"column"}
           ></Cards>
-           <h6>Design de l’Interface Utilisateur (UI/UX)</h6>
+           <h3>Design de l’Interface Utilisateur (UI/UX)</h3>
         <Cards
             img={prototype}
             alt={"ddd"}
@@ -45,47 +45,47 @@ function SoftwareOffres()
             width={"100%"}
             flex={"column"}
           ></Cards>
-           <h6>Développement du Code Source</h6>
+           <h3>Développement du Code Source</h3>
         <Cards
             img={codeSource}
             alt={"ddd"}
-            txt={"Nous écrivons le code HTML, CSS, et JavaScript pour l'interface  utilisateur. Nous utilisons des frameworks comme React ou Angular si  nécessaire pour améliorer l'expérience utilisateur."}
+            txt={"Nous écrivons le code en utilisant des langages de programmation adaptés (Java, C++, Python, etc.). Nous utilisons des frameworks et  bibliothèques pour accélérer le développement tout en maintenant une  qualité élevée."}
             height={82}
             width={"100%"}
             flex={"column"}
           ></Cards>
-           <h6>Développement Back-end </h6>
+           <h3>Développement Back-end </h3>
         <Cards
             img={backend}
             alt={"ddd"}
-            txt={"Nous créons l'API ou le serveur web avec des langages comme PHP ou  Python. Nous utilisons des frameworks comme Laravel ou Django pour  assurer une robustesse et une flexibilité maximales."}
+            txt={"Nous créons l'API ou le serveur avec des langages comme PHP ou  Python. Nous utilisons des frameworks comme Laravel ou Django pour  assurer une robustesse et une flexibilité maximales."}
             height={82}
             width={"100%"}
             flex={"column"}
           ></Cards>
-           <h6>Intégration de la Base de Données </h6>
+           <h3>Intégration de la Base de Données </h3>
         <Cards
             img={bdd}
             alt={"ddd"}
-            txt={"Nous configurons et intégrons une base de données (MySQL, PostgreSQL,  MongoDB, etc.) pour stocker et gérer vos données de manière sécurisée."}
+            txt={"Nous configurons et intégrons une base de données pour stocker et gérer vos données de manière sécurisée."}
             height={82}
             width={"100%"}
             flex={"column"}
           ></Cards>
-           <h6>Tests et Débogage</h6>
+           <h3>Tests et Débogage</h3>
         <Cards
             img={debug}
             alt={"ddd"}
-            txt={"Nous effectuons des tests unitaires, d'intégration, et fonctionnels pour garantir que votre site fonctionne parfaitement. Nous corrigons les  bugs et optimisons les performances pour une expérience utilisateur  fluide."}
+            txt={"Nous effectuons des tests unitaires, d'intégration, et de système pour  garantir que votre logiciel fonctionne parfaitement. Nous identifions et corrigeons les bugs pour une expérience utilisateur fluide."}
             height={82}
             width={"100%"}
             flex={"column"}
           ></Cards>
-           <h6>Déploiement</h6>
+           <h3>Déploiement</h3>
         <Cards
             img={deploiement}
             alt={"ddd"}
-            txt={"Nous mettons en ligne votre site sur un serveur web sécurisé (AWS,  Google Cloud, etc.). Nous configurons les certificats SSL pour assurer  une navigation sécurisée."}
+            txt={"Nous mettons le logiciel à disposition des utilisateurs via un  installateur ou un package. Nous configurons les mécanismes de mise à  jour automatique si nécessaire pour assurer une expérience continue."}
             height={82}
             width={"100%"}
             flex={"column"}
